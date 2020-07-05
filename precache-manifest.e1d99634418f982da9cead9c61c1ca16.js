2d5c2ae4c89e12125ebf6c9ad446b0ccb6a347fc
JerryKaura <jerimkaura001@gmail.com>
2020-07-05 13:37:43 +0300

    Updates

---
+++ precache-manifest.e1d99634418f982da9cead9c61c1ca16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186508a0e01bebc13581749bfd37d9c8",
+    "url": "/JerryKaura.github.io/first-react-app/index.html"
+  },
+  {
+    "revision": "002350899625ccb0078f",
+    "url": "/JerryKaura.github.io/first-react-app/static/css/main.563df576.chunk.css"
+  },
+  {
+    "revision": "fd09f2bb3969c2bf53ce",
+    "url": "/JerryKaura.github.io/first-react-app/static/js/2.b461cd9c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JerryKaura.github.io/first-react-app/static/js/2.b461cd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "002350899625ccb0078f",
+    "url": "/JerryKaura.github.io/first-react-app/static/js/main.bb922e81.chunk.js"
+  },
+  {
+    "revision": "6c740537c560b0ef2136",
+    "url": "/JerryKaura.github.io/first-react-app/static/js/runtime-main.d715fc85.js"
+  }
+]);
